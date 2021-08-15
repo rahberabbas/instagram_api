3345927c5bd3b75fd4c10daec1968ec2dcb4dc99
rahberabbas <amanabidi1234@gmail.com>
2021-08-15 16:57:44 +0530

    last commit

--- apis/views.py
+++ apis/views.py
@@ -8,6 +8,8 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 #options.headless = True
 options.add_argument("--window-size=1920,1080")
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
 # options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
