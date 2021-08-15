7e9c85da5c18a59b486b80008eb4ff145f16ed74
rahberabbas <amanabidi1234@gmail.com>
2021-08-15 20:37:04 +0530

    hello

--- instagram/main.py
+++ instagram/main.py
@@ -11,6 +11,7 @@ options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-gpu")
+options.add_argument('--remote-debugging-port=9222')
 
 
 main_url = 'https://www.instagram.com/tv/CKtHy7RhPh4/?__a=1'
