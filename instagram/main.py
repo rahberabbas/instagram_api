23b01599751a34891c94fa8cf18760584710deb8
rahberabbas <amanabidi1234@gmail.com>
2021-08-15 20:23:44 +0530

    last commit

--- instagram/main.py
+++ instagram/main.py
@@ -8,15 +8,14 @@ options.add_argument("--window-size=1920,1080")
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+# options.add_argument("--disable-gpu")
 
 
-main_url = 'https://instafinsta.com/insta-dp-viewer'
-driver = webdriver.Chrome(options=options)
+main_url = 'https://www.instagram.com/tv/CKtHy7RhPh4/?__a=1'
+driver = webdriver.Chrome(executable_path='/Users/rahberabbasabidi/Desktop/Instagram_API/chromedriver', options=options)
 driver.get(main_url)
-driver.find_element_by_name('link')
-print("OK")
-# # json_text = driver.find_element_by_css_selector('pre').get_attribute('innerText')
+print(driver.find_element_by_xpath("/html/body").text)
+# json_text = driver.find_element_by_css_selector('pre').get_attribute('innerText')
 # if json_text:
 #     r = json.loads(json_text)
 #     video_url = r['graphql']['shortcode_media']['video_url']
