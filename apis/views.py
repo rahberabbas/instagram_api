921d28f0ef7c938fbf78eabeb65e5590d2a32cf6
rahberabbas <amanabidi1234@gmail.com>
2021-08-15 20:37:39 +0530

    hello

--- apis/views.py
+++ apis/views.py
@@ -11,8 +11,11 @@ options.add_argument("--window-size=1920,1080")
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument('--no-sandbox')
 options.add_argument("--disable-gpu")
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+options.add_argument('--remote-debugging-port=9222')
+# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 headers = {
             'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
         }
